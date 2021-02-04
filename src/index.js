007b8409fd090ff0340774b3f7b317c3f4c54972
beppe11 <78246566+beppe11@users.noreply.github.com>
2021-02-05 07:50:31 +0900

    削除機能実装

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ const onClickAdd = () => {
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("delete");
+    const deleteTarget = deleteButton.parentNode;
+    document.querySelector("#incomplete-list").removeChild(deleteTarget);
   });
   // divタグの子要素に各要素を追加
   div.appendChild(li);
