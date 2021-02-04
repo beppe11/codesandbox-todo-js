e87286b9406bec47c702840f0bb5d96d75be82bf
beppe11 <78246566+beppe11@users.noreply.github.com>
2021-02-04 11:07:50 +0900

    タスクの追加実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.querySelector("#add-text").value;
+  document.querySelector("#add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("complete");
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("delete");
+  });
+  // divタグの子要素に各要素を追加
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  document.querySelector("#incomplete-list").appendChild(div);
+};
+
+document
+  .querySelector("#add-button")
+  .addEventListener("click", () => onClickAdd());
